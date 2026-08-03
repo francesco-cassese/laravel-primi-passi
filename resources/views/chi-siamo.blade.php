98248c30a5415ec967946086ccfd2bd6c2714a25
Francesco Cassese <francescocassese9@gmail.com>
2026-08-03 12:42:52 +0200

    feat: add chi-siamo and contatti pages with named routes

---
+++ resources/views/chi-siamo.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel-primi-passi</title>
+</head>
+
+<body>
+
+    @include('partials.header')
+
+    <h1>
+        {{ $titolo }}
+    </h1>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,35 @@
 
 use Illuminate\Support\Facades\Route;
 
+// --- Homepage ---
+
 Route::get('/', function () {
 
-    $titolo_pagina = "Hello world!";
+    $titolo_pagina = "Homepage";
 
     return view('home', [
         "titolo" => $titolo_pagina
     ]);
-});
+})->name('home');
+
+// --- ChiSiamo ---
+
+Route::get('/chi-siamo', function () {
+
+    $titolo_pagina = "Chi siamo";
+
+    return view('chi-siamo', [
+        "titolo" => $titolo_pagina
+    ]);
+})->name('chi-siamo');
+
+// --- Contatti ---
+
+Route::get('/contatti', function () {
+
+    $titolo_pagina = "Contatti";
+
+    return view('contatti', [
+        "titolo" => $titolo_pagina
+    ]);
+})->name('contatti');
